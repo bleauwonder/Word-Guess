2f71d7a363671ef01a8874500a7e842e02ce1acd
bleauwonder <elyzableau.om@gmail.com>
2019-05-04 09:54:15 -0400

    fixed getBlanks, finished updateRound, hasWon, hasLost

--- Archive/logic.js
+++ Archive/logic.js
@@ -2,9 +2,11 @@
 var gameWords = ["probe", "hoax", "roswell", "coneheads", "futurama", "mork", "xenomorph", "alf", "ewok", "reptilian", "gamorean"];
 // var letters = ["a", "a", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "_"];
 var blanksAndCorrect = [];
+var ansWordArr = [];
+var randomIndex;
 //Random word function
 function randomWord(gameWords) {
-    var randomIndex = Math.floor(Math.random() * gameWords.length);
+    randomIndex = Math.floor(Math.random() * gameWords.length);
     return gameWords[randomIndex];
 }
 
@@ -17,24 +19,60 @@ var isCorrectGuess = function(word, letter) {
     return false;
 }
 
-function getBlanks(word) {
-    //computer generates random word from words array
-    var blanksArr = word.split("");
-    //Looping to generate "_" for each letter in array stored in blanks
-    for (var i = 0; i < blanksArr.length; i++) {
-        blanksArr[i] = "_";
-    }
-    return blanksArr;    
+var getBlanks = function(word) {
+        // adds "_" to ansWordArr
+        for (var i = 0; i < word.length; i++) {
+            ansWordArr[i] = "_";
+        }
+        return ansWordArr; 
 }
+
 // Replace the blank with a correctly guessed letter
 function fillBlanks(word, puzzleState, letter) {
     if(isCorrectGuess(word, letter)){
-        for (var i = 3; i < word.length; i++) {
-           if(word[i]===letter) {
+        for (var i = 0; i < word.length; i++) {
+           if(word[i] === letter) {
                puzzleState[i] = letter;
            }   
         }
     }
    return puzzleState;
 }
+// Game Management Functions \\
 
+function setupRound(word) {
+    var obj = {
+        word:word,
+        guessesLeft: 9,
+        wrongGuesses:[],
+        puzzleState: getBlanks(word),
+    }
+    return obj;
+ }
+
+function updateRound(obj, letter) {
+     if (isCorrectGuess(obj.word, letter) === false) {
+         obj.guessesLeft--;
+         obj.wrongGuesses.push(letter);
+     }
+     else {
+         fillBlanks(obj.word, obj.puzzleState, letter)
+     }
+     return obj;
+}
+
+function hasWon(puzzleState) {
+    for (var i = 0; i < puzzleState.length; i++) {
+    if (puzzleState[i] === "_") {
+        return false;
+     } 
+    }
+    return true;
+}
+
+function hasLost(guessesLeft) {
+        if (guessesLeft === 0) {
+            return true;
+        }
+        return false;
+}
